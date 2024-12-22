4292daa4cb64e79ebbbb25343f2aa7b1c6bca267
edrysse <117366780+edrysse@users.noreply.github.com>
2024-12-22 16:39:03 +0100

    head secure

--- resources/views/client/includes/head.blade.php
+++ resources/views/client/includes/head.blade.php
@@ -1,65 +1,68 @@
-<head>
-    
+    <!--  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    {{-- <title>Merci Laayoune</title>
-    <meta name="description" content="Merci Laayoune est un élégant café salon de thé avec une charmante terrasse 
-        située au cœur de la ville de Laayoune. Notre ambiance chaleureuse et notre menu raffiné vous invitent à vous
-        détendre et à savourer des moments de convivialité. Profitez de notre vaste sélection de boissons chaudes et froides, 
-        de délicieuses pâtisseries fraîchement préparées et de plats légers. Que ce soit pour une pause délicieuse ou une 
-        rencontre entre amis, Merci Laayoune vous accueille avec un service attentionné et un cadre accueillant."> --}}
-        @yield('meta')
+    @yield('meta')
 
-    
-    <!--===============================================================================================-->
-    {{-- <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet"> --}}
-    <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href={{ asset('clientpage/images/MERCI_IMG/logo_header/Logo-Merci-b3.png') }} />
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href={{ asset('clientpage/vendor/bootstrap/css/bootstrap.min.css') }}>
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css"
-        href={{ asset('clientpage/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}>
-    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="{{ secure_asset('clientpage/images/MERCI_IMG/logo_header/Logo-Merci-b3.png') }}" />
 
-    <link rel="stylesheet" type="text/css" href={{ asset('clientpage/fonts/themify/themify-icons.css') }}>
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href={{ asset('clientpage/vendor/animate/animate.css') }}>
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href={{ asset('clientpage/vendor/css-hamburgers/hamburgers.min.css') }}>
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href={{ asset('clientpage/vendor/animsition/css/animsition.min.css') }}>
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href={{ asset('clientpage/vendor/select2/select2.min.css') }}>
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href={{ asset('clientpage/vendor/daterangepicker/daterangepicker.css') }}>
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href={{ asset('clientpage/vendor/slick/slick.css') }}>
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href={{ asset('clientpage/vendor/lightbox2/css/lightbox.min.css') }}>
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href={{ asset('clientpage/css/util.css') }}>
-    <link rel="stylesheet" type="text/css" href={{ asset('clientpage/css/main.css') }}>
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href={{ asset('clientpage/css/all.min.css') }}>
-    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/fonts/themify/themify-icons.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/animate/animate.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/css-hamburgers/hamburgers.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/animsition/css/animsition.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/select2/select2.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/daterangepicker/daterangepicker.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/slick/slick.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/lightbox2/css/lightbox.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/css/util.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/css/main.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/css/all.min.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.css" integrity="sha256-NAxhqDvtY0l4xn+YVa6WjAcmd94NNfttjNsDmNatFVc=" crossorigin="anonymous" />
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <!-- Fontawesome CDN Link -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@3.6.12/dist/css/splide.min.css">
+</head>
+-->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @yield('meta')
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@3.6.12/dist/css/splide.min.css">
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="{{ secure_asset('clientpage/images/MERCI_IMG/logo_header/Logo-Merci-b3.png') }}" />
 
+    <!-- Stylesheets -->
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/fonts/themify/themify-icons.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/animate/animate.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/css-hamburgers/hamburgers.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/animsition/css/animsition.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/select2/select2.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/daterangepicker/daterangepicker.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/slick/slick.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/lightbox2/css/lightbox.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/css/util.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/css/main.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/css/all.min.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.min.css" integrity="sha256-NAxhqDvtY0l4xn+YVa6WjAcmd94NNfttjNsDmNatFVc=" crossorigin="anonymous" />
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha256-XYZSecureHashHere" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@3.6.12/dist/css/splide.min.css">
 
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-KyZXEAg3QhqLMpG8r+Knujsl5/5hb7VV91Nj75daVUwDIZKtKoSZj3txMIdGBoc9" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@2.11.8/dist/umd/popper.min.js" integrity="sha384-Q6+PopperSecureHashHere" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+BootstrapSecureHashHere" crossorigin="anonymous"></script>
 </head>
