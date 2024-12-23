6da17f20ac99bc4f417c58cae0823d3967187056
edrysse <117366780+edrysse@users.noreply.github.com>
2024-12-23 03:14:54 +0100

    m

--- resources/views/admin/menu/brunches/index.blade.php
+++ resources/views/admin/menu/brunches/index.blade.php
@@ -45,7 +45,7 @@ Liste des Brunchs    </h2>
                     <td>{{ $brunch->nom }}</td>
                     <td>{{ \Illuminate\Support\Str::limit($brunch->description, 50) }}</td>
                     <td>{{ $brunch->prix }} MAD</td>
-                    <td><img src="{{ asset('storage/' . $brunch->image) }}" alt="Image" width="100"></td>
+                    <td><img src="{{ secure_asset('storage/' . $brunch->image) }}" alt="Image" width="100"></td>
                     <td>
                         <a href="#" class="btn btn-danger">Supprimer</a>
                     </td>
